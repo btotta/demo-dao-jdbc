ebcee4f3f46c43df4c75e836805454651d209f32
btotta <bruno.ttota@gmail.com>
2020-05-26 21:27:17 -0300

    update impl

--- src/application/Program.java
+++ src/application/Program.java
@@ -39,12 +39,17 @@ public class Program {
 			System.out.println(obj);
 		}
 
-		
 		System.out.println("-- teste 4 -- seller insert");
-		
+
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted new seller! " + newSeller.getId());
+
+		System.out.println("-- teste 5 -- seller update");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha W");
+		sellerDao.update(seller);
+		System.out.println("Update completed!");
 	}
 
 }
